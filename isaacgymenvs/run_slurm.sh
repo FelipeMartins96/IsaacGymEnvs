5755f7b22f414f84f91de6227067c9de90faf745
Felipe Martins <felipemartins96@gmail.com>
2022-11-13 23:49:06 -0300

    wip

---
+++ isaacgymenvs/run_slurm.sh
@@ -0,0 +1,2 @@
+python train.py task=VSS task.env.has_grad=False experiment=VSS headless=True max_iterations=1000;
+python train.py task=VSS task.env.has_grad=True experiment=VSS+grad headless=True max_iterations=1000
