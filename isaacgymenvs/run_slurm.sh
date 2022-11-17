9c93f20c9f1a111e93f90246c18a187f32eb56b0
Felipe Martins <felipemartins96@gmail.com>
2022-11-17 16:25:16 -0300

    experiment: 3v3 with no OU and no ball_vell

--- isaacgymenvs/run_slurm.sh
+++ isaacgymenvs/run_slurm.sh
@@ -1,6 +1,4 @@
 case $1 in
-0) python train.py task=VSS experiment=3v3VSS42;;
-1) python train.py task=VSS experiment=3v3VSS43 seed=43;;
-2) python train.py task=VSS experiment=3v3VSS44 seed=44;;
+0) python train.py task=VSS experiment=3v3-noOU-noBallVel task.env.has_initial_ball_vel=False task.env.has_env_ou_noise=False;;
 *) echo "Opcao Invalida!" ;;
 esac
