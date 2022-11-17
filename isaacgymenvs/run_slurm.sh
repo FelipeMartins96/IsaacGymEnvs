1cfc7cc066342f33aee2ca761fada32e44ddebc0
Felipe Martins <felipemartins96@gmail.com>
2022-11-17 17:05:38 -0300

    experiment: bigger net

--- isaacgymenvs/run_slurm.sh
+++ isaacgymenvs/run_slurm.sh
@@ -1,4 +1,4 @@
 case $1 in
-0) python train.py task=VSS experiment=3v3-noOU-noBallVel task.env.has_initial_ball_vel=False task.env.has_env_ou_noise=False;;
+0) python train.py task=VSS experiment=3v3-biggerNet;;
 *) echo "Opcao Invalida!" ;;
 esac
