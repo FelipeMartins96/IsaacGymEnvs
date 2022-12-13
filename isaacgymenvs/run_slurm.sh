f3be294d194ecc543d71dc0cc6e9650c5f00cf6c
Felipe Martins <fbm2@cin.ufpe.br>
2022-12-13 18:51:41 -0300

    slurm experiment script

--- isaacgymenvs/run_slurm.sh
+++ isaacgymenvs/run_slurm.sh
@@ -1,6 +1,7 @@
 case $1 in
-0) python train.py task=VSS experiment=ppo-CentralizedMA-meanMove-$1 seed=$1;;
-1) python train.py task=VSS experiment=ppo-CentralizedMA-meanMove-$1 seed=$1;;
-2) python train.py task=VSS experiment=ppo-CentralizedMA-meanMove-$1 seed=$1;;
+0) python train.py task=VSS experiment=ppo-base;;
+1) python train.py task=VSS train.params.config.gamma=0.95 experiment=ppo-gamma0.95;;
+2) python train.py task=VSS task.env.has_initial_ball_vel=False experiment=ppo-noInitBallVel;;
+3) python train.py task=VSS task.env.has_initial_ball_vel=False task.env.has_env_ou_noise=False experiment=ppo-noInitBallVelAndOU;;
 *) echo "Opcao Invalida!" ;;
 esac
