bedfac4aaf8549c25f87ff62214af1aca95ec84f
Felipe Martins <felipemartins96@gmail.com>
2022-12-15 13:51:48 -0300

    no energy rw experiment

--- experiments/run_slurm.sh
+++ experiments/run_slurm.sh
@@ -1,15 +1,15 @@
 case $1 in
 0) python train_task.py seed=42 task=VSS experiment=ddpg-cma-base;;
 1) python train_task.py seed=42 task=VSS train.params.config.gamma=0.95 experiment=ddpg-cma-gamma0.95;;
-2) python train_task.py seed=42 task=VSS task.env.has_initial_ball_vel=False experiment=ddpg-cma-noInitBallVel;;
+2) python train_task.py seed=42 task=VSS task.env.has_initial_ball_vel=False experiment=ddpg-cma-noInitBallVel-noEnergy;;
 3) python train_task.py seed=42 task=VSS task.env.has_initial_ball_vel=False task.env.has_env_ou_noise=False experiment=ddpg-cma-noInitBallVelAndOU;;
 4) python train_task.py seed=43 task=VSS experiment=ddpg-cma-base;;
 5) python train_task.py seed=43 task=VSS train.params.config.gamma=0.95 experiment=ddpg-cma-gamma0.95;;
-6) python train_task.py seed=43 task=VSS task.env.has_initial_ball_vel=False experiment=ddpg-cma-noInitBallVel;;
+6) python train_task.py seed=43 task=VSS task.env.has_initial_ball_vel=False experiment=ddpg-cma-noInitBallVel-noEnergy;;
 7) python train_task.py seed=43 task=VSS task.env.has_initial_ball_vel=False task.env.has_env_ou_noise=False experiment=ddpg-cma-noInitBallVelAndOU;;
 8) python train_task.py seed=44 task=VSS experiment=ddpg-cma-base;;
 9) python train_task.py seed=44 task=VSS train.params.config.gamma=0.95 experiment=ddpg-cma-gamma0.95;;
-10) python train_task.py seed=44 task=VSS task.env.has_initial_ball_vel=False experiment=ddpg-cma-noInitBallVel;;
+10) python train_task.py seed=44 task=VSS task.env.has_initial_ball_vel=False experiment=ddpg-cma-noInitBallVel-noEnergy;;
 11) python train_task.py seed=44 task=VSS task.env.has_initial_ball_vel=False task.env.has_env_ou_noise=False experiment=ddpg-cma-noInitBallVelAndOU;;
 *) echo "Opcao Invalida!" ;;
 esac
--- isaacgymenvs/run_slurm.sh
+++ isaacgymenvs/run_slurm.sh
@@ -1,15 +1,15 @@
 case $1 in
 0) python train.py task=VSS seed=42 experiment=ppo-cma-base;;
 1) python train.py task=VSS seed=42 train.params.config.gamma=0.95 experiment=ppo-cma-gamma0.95;;
-2) python train.py task=VSS seed=42 task.env.has_initial_ball_vel=False experiment=ppo-cma-noInitBallVel;;
+2) python train.py task=VSS seed=42 task.env.has_initial_ball_vel=False experiment=ppo-cma-noInitBallVel-noEnergy;;
 3) python train.py task=VSS seed=42 task.env.has_initial_ball_vel=False task.env.has_env_ou_noise=False experiment=ppo-cma-noInitBallVelAndOU;;
 4) python train.py task=VSS seed=43 experiment=ppo-cma-base;;
 5) python train.py task=VSS seed=43 train.params.config.gamma=0.95 experiment=ppo-cma-gamma0.95;;
-6) python train.py task=VSS seed=43 task.env.has_initial_ball_vel=False experiment=ppo-cma-noInitBallVel;;
+6) python train.py task=VSS seed=43 task.env.has_initial_ball_vel=False experiment=ppo-cma-noInitBallVel-noEnergy;;
 7) python train.py task=VSS seed=43 task.env.has_initial_ball_vel=False task.env.has_env_ou_noise=False experiment=ppo-cma-noInitBallVelAndOU;;
 8) python train.py task=VSS seed=44 experiment=ppo-cma-base;;
 9) python train.py task=VSS seed=44 train.params.config.gamma=0.95 experiment=ppo-cma-gamma0.95;;
-10) python train.py task=VSS seed=44 task.env.has_initial_ball_vel=False experiment=ppo-cma-noInitBallVel;;
+10) python train.py task=VSS seed=44 task.env.has_initial_ball_vel=False experiment=ppo-cma-noInitBallVel-noEnergy;;
 11) python train.py task=VSS seed=44 task.env.has_initial_ball_vel=False task.env.has_env_ou_noise=False experiment=ppo-cma-noInitBallVelAndOU;;
 *) echo "Opcao Invalida!" ;;
 esac
