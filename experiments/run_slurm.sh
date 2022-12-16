a103d41ae37dc412ec6f680b132df4fd6d00880c
Felipe Martins <felipemartins96@gmail.com>
2022-12-16 00:19:49 -0300

    increase speed scheduler to 50k steps

--- experiments/run_slurm.sh
+++ experiments/run_slurm.sh
@@ -1,9 +1,9 @@
 case $1 in
-0) python train_task.py task=VSS seed=42 experiment=ddpg-base;;
-1) python train_task.py task=VSS seed=43 experiment=ddpg-base;;
-2) python train_task.py task=VSS seed=44 experiment=ddpg-base;;
-3) python train_task.py task=VSS seed=42 task.env.n_controlled_robots=3 experiment=ddpg-cma-base;;
-4) python train_task.py task=VSS seed=43 task.env.n_controlled_robots=3 experiment=ddpg-cma-base;;
-5) python train_task.py task=VSS seed=44 task.env.n_controlled_robots=3 experiment=ddpg-cma-base;;
+0) python train_task.py task=VSS seed=42 experiment=ddpg-base-50k;;
+1) python train_task.py task=VSS seed=43 experiment=ddpg-base-50k;;
+2) python train_task.py task=VSS seed=44 experiment=ddpg-base-50k;;
+3) python train_task.py task=VSS seed=42 task.env.n_controlled_robots=3 experiment=ddpg-cma-base-50k;;
+4) python train_task.py task=VSS seed=43 task.env.n_controlled_robots=3 experiment=ddpg-cma-base-50k;;
+5) python train_task.py task=VSS seed=44 task.env.n_controlled_robots=3 experiment=ddpg-cma-base-50k;;
 *) echo "Opcao Invalida!" ;;
 esac
--- isaacgymenvs/run_slurm.sh
+++ isaacgymenvs/run_slurm.sh
@@ -1,9 +1,9 @@
 case $1 in
-0) python train.py task=VSS seed=42 experiment=ppo-base;;
-1) python train.py task=VSS seed=43 experiment=ppo-base;;
-2) python train.py task=VSS seed=44 experiment=ppo-base;;
-3) python train.py task=VSS seed=42 task.env.n_controlled_robots=3 experiment=ppo-cma-base;;
-4) python train.py task=VSS seed=43 task.env.n_controlled_robots=3 experiment=ppo-cma-base;;
-5) python train.py task=VSS seed=44 task.env.n_controlled_robots=3 experiment=ppo-cma-base;;
+0) python train.py task=VSS seed=42 experiment=ppo-base-50k;;
+1) python train.py task=VSS seed=43 experiment=ppo-base-50k;;
+2) python train.py task=VSS seed=44 experiment=ppo-base-50k;;
+3) python train.py task=VSS seed=42 task.env.n_controlled_robots=3 experiment=ppo-cma-base-50k;;
+4) python train.py task=VSS seed=43 task.env.n_controlled_robots=3 experiment=ppo-cma-base-50k;;
+5) python train.py task=VSS seed=44 task.env.n_controlled_robots=3 experiment=ppo-cma-base-50k;;
 *) echo "Opcao Invalida!" ;;
 esac
