535af4f4087383cdbb71f8924c6ebedee8766c0a
Felipe Martins <felipemartins96@gmail.com>
2022-12-19 11:13:14 -0300

    experiment scripts

--- isaacgymenvs/run_slurm.sh
+++ isaacgymenvs/run_slurm.sh
@@ -1,6 +1,18 @@
 case $1 in
-3) python train.py task=VSS seed=42 task.env.n_controlled_robots=3 experiment=ppo-cma-adaptative-100k;;
-4) python train.py task=VSS seed=43 task.env.n_controlled_robots=3 experiment=ppo-cma-adaptative-100k;;
-5) python train.py task=VSS seed=44 task.env.n_controlled_robots=3 experiment=ppo-cma-adaptative-100k;;
+0) python train.py task=VSS seed=40 experiment=ppo;;
+1) python train.py task=VSS seed=41 experiment=ppo;;
+2) python train.py task=VSS seed=42 experiment=ppo;;
+3) python train.py task=VSS seed=43 experiment=ppo;;
+4) python train.py task=VSS seed=44 experiment=ppo;;
+5) python train.py task=VSS seed=40 task.env.n_controlled_robots=3 experiment=ppo-dma;;
+6) python train.py task=VSS seed=41 task.env.n_controlled_robots=3 experiment=ppo-dma;;
+7) python train.py task=VSS seed=42 task.env.n_controlled_robots=3 experiment=ppo-dma;;
+8) python train.py task=VSS seed=43 task.env.n_controlled_robots=3 experiment=ppo-dma;;
+9) python train.py task=VSS seed=44 task.env.n_controlled_robots=3 experiment=ppo-dma;;
+10) python train.py task=VSSDecentralizedMA seed=40 experiment=ppo-dma;;
+11) python train.py task=VSSDecentralizedMA seed=41 experiment=ppo-dma;;
+12) python train.py task=VSSDecentralizedMA seed=42 experiment=ppo-dma;;
+13) python train.py task=VSSDecentralizedMA seed=43 experiment=ppo-dma;;
+14) python train.py task=VSSDecentralizedMA seed=44 experiment=ppo-dma;;
 *) echo "Opcao Invalida!" ;;
 esac
