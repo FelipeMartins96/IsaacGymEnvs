23772a5e3c3851c462322bb83653ee782e4a7c2f
Felipe Martins <felipemartins96@gmail.com>
2022-12-20 11:42:02 -0300

    run dirs test

--- isaacgymenvs/run_slurm.sh
+++ isaacgymenvs/run_slurm.sh
@@ -1,18 +1,18 @@
 case $1 in
-0) python train.py task=VSS seed=40 experiment=ppo-40;;
-1) python train.py task=VSS seed=41 experiment=ppo-41;;
-2) python train.py task=VSS seed=42 experiment=ppo-42;;
-3) python train.py task=VSS seed=43 experiment=ppo-43;;
-4) python train.py task=VSS seed=44 experiment=ppo-44;;
-5) python train.py task=VSS seed=40 task.env.n_controlled_robots=3 experiment=ppo-dma-40;;
-6) python train.py task=VSS seed=41 task.env.n_controlled_robots=3 experiment=ppo-dma-41;;
-7) python train.py task=VSS seed=42 task.env.n_controlled_robots=3 experiment=ppo-dma-42;;
-8) python train.py task=VSS seed=43 task.env.n_controlled_robots=3 experiment=ppo-dma-43;;
-9) python train.py task=VSS seed=44 task.env.n_controlled_robots=3 experiment=ppo-dma-44;;
-10) python train.py task=VSSDecentralizedMA seed=40 experiment=ppo-cma-40;;
-11) python train.py task=VSSDecentralizedMA seed=41 experiment=ppo-cma-41;;
-12) python train.py task=VSSDecentralizedMA seed=42 experiment=ppo-cma-42;;
-13) python train.py task=VSSDecentralizedMA seed=43 experiment=ppo-cma-43;;
-14) python train.py task=VSSDecentralizedMA seed=44 experiment=ppo-cma-44;;
+0) python train.py task=VSS seed=40 experiment=ppo;;
+1) python train.py task=VSS seed=41 experiment=ppo;;
+2) python train.py task=VSS seed=42 experiment=ppo;;
+3) python train.py task=VSS seed=43 experiment=ppo;;
+4) python train.py task=VSS seed=44 experiment=ppo;;
+5) python train.py task=VSS seed=40 task.env.n_controlled_robots=3 experiment=ppo-dma;;
+6) python train.py task=VSS seed=41 task.env.n_controlled_robots=3 experiment=ppo-dma;;
+7) python train.py task=VSS seed=42 task.env.n_controlled_robots=3 experiment=ppo-dma;;
+8) python train.py task=VSS seed=43 task.env.n_controlled_robots=3 experiment=ppo-dma;;
+9) python train.py task=VSS seed=44 task.env.n_controlled_robots=3 experiment=ppo-dma;;
+10) python train.py task=VSSDecentralizedMA seed=40 experiment=ppo-cma;;
+11) python train.py task=VSSDecentralizedMA seed=41 experiment=ppo-cma;;
+12) python train.py task=VSSDecentralizedMA seed=42 experiment=ppo-cma;;
+13) python train.py task=VSSDecentralizedMA seed=43 experiment=ppo-cma;;
+14) python train.py task=VSSDecentralizedMA seed=44 experiment=ppo-cma;;
 *) echo "Opcao Invalida!" ;;
 esac
--- isaacgymenvs/train.py
+++ isaacgymenvs/train.py
@@ -59,7 +59,8 @@ def launch_rlg_hydra(cfg: DictConfig):
     import isaacgymenvs
 
     time_str = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    run_name = cfg.wandb_name
+    run_name = f"{cfg.wandb_name}_{cfg.seed}"
+    cfg.train.params.config.name = run_name
     
     # ensure checkpoints can be specified as relative paths
     if cfg.checkpoint:
