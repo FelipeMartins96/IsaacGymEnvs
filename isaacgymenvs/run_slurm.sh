d11056c1252791767dfd1bef004e319a67cd1b0f
Felipe Martins <fbm2@cin.ufpe.br>
2022-12-20 16:30:08 -0300

    fix dma lr, and dma/cma naming mixup

--- isaacgymenvs/run_slurm.sh
+++ isaacgymenvs/run_slurm.sh
@@ -4,15 +4,15 @@ case $1 in
 2) python train.py task=VSS seed=42 experiment=ppo;;
 3) python train.py task=VSS seed=43 experiment=ppo;;
 4) python train.py task=VSS seed=44 experiment=ppo;;
-5) python train.py task=VSS seed=40 task.env.n_controlled_robots=3 experiment=ppo-dma;;
-6) python train.py task=VSS seed=41 task.env.n_controlled_robots=3 experiment=ppo-dma;;
-7) python train.py task=VSS seed=42 task.env.n_controlled_robots=3 experiment=ppo-dma;;
-8) python train.py task=VSS seed=43 task.env.n_controlled_robots=3 experiment=ppo-dma;;
-9) python train.py task=VSS seed=44 task.env.n_controlled_robots=3 experiment=ppo-dma;;
-10) python train.py task=VSSDecentralizedMA seed=40 experiment=ppo-cma;;
-11) python train.py task=VSSDecentralizedMA seed=41 experiment=ppo-cma;;
-12) python train.py task=VSSDecentralizedMA seed=42 experiment=ppo-cma;;
-13) python train.py task=VSSDecentralizedMA seed=43 experiment=ppo-cma;;
-14) python train.py task=VSSDecentralizedMA seed=44 experiment=ppo-cma;;
+5) python train.py task=VSS seed=40 task.env.n_controlled_robots=3 experiment=ppo-cma;;
+6) python train.py task=VSS seed=41 task.env.n_controlled_robots=3 experiment=ppo-cma;;
+7) python train.py task=VSS seed=42 task.env.n_controlled_robots=3 experiment=ppo-cma;;
+8) python train.py task=VSS seed=43 task.env.n_controlled_robots=3 experiment=ppo-cma;;
+9) python train.py task=VSS seed=44 task.env.n_controlled_robots=3 experiment=ppo-cma;;
+10) python train.py task=VSSDecentralizedMA seed=40 experiment=ppo-dma;;
+11) python train.py task=VSSDecentralizedMA seed=41 experiment=ppo-dma;;
+12) python train.py task=VSSDecentralizedMA seed=42 experiment=ppo-dma;;
+13) python train.py task=VSSDecentralizedMA seed=43 experiment=ppo-dma;;
+14) python train.py task=VSSDecentralizedMA seed=44 experiment=ppo-dma;;
 *) echo "Opcao Invalida!" ;;
 esac
