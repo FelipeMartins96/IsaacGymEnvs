f51a06e5c2b263b5ac1b1982aa90a86a239b1271
Felipe Martins <fbm2@cin.ufpe.br>
2023-02-13 15:41:01 -0300

    exp001

--- isaacgymenvs/run_slurm.sh
+++ isaacgymenvs/run_slurm.sh
@@ -1,6 +1,12 @@
 case $1 in
-0) python train.py task=VSS seed=40 experiment=ppo wandb_group=teste;;
-1) python train.py task=VSS seed=40 task.env.n_controlled_robots=3 experiment=ppo-cma wandb_group=teste;;
-2) python train.py task=VSSDecentralizedMA seed=40 experiment=ppo-dma wandb_group=teste;;
+0) python train.py task=VSS seed=10 experiment=ppo wandb_group=exp001;;
+1) python train.py task=VSS seed=20 experiment=ppo wandb_group=exp001;;
+2) python train.py task=VSS seed=30 experiment=ppo wandb_group=exp001;;
+3) python train.py task=VSS seed=10 experiment=ppo-cma wandb_group=exp001 task.env.n_controlled_robots=3;;
+4) python train.py task=VSS seed=20 experiment=ppo-cma wandb_group=exp001 task.env.n_controlled_robots=3;;
+5) python train.py task=VSS seed=30 experiment=ppo-cma wandb_group=exp001 task.env.n_controlled_robots=3;;
+6) python train.py task=VSSDecentralizedMA seed=10 experiment=ppo-dma wandb_group=exp001;;
+7) python train.py task=VSSDecentralizedMA seed=20 experiment=ppo-dma wandb_group=exp001;;
+8) python train.py task=VSSDecentralizedMA seed=30 experiment=ppo-dma wandb_group=exp001;;
 *) echo "Opcao Invalida!" ;;
 esac
