79c6853445739ff51d866963379c05ed3e0eacd7
Felipe Martins <felipemartins96@gmail.com>
2023-02-17 17:01:44 -0300

    experiment 4: robot physics ablation

--- isaacgymenvs/run_slurm.sh
+++ isaacgymenvs/run_slurm.sh
@@ -1,12 +1,12 @@
 case $1 in
-0) python train.py task=VSS seed=10 experiment=ppo wandb_group=exp002;;
-1) python train.py task=VSS seed=20 experiment=ppo wandb_group=exp002;;
-2) python train.py task=VSS seed=30 experiment=ppo wandb_group=exp002;;
-3) python train.py task=VSS seed=10 experiment=ppo-cma wandb_group=exp002 task.env.n_controlled_robots=3;;
-4) python train.py task=VSS seed=20 experiment=ppo-cma wandb_group=exp002 task.env.n_controlled_robots=3;;
-5) python train.py task=VSS seed=30 experiment=ppo-cma wandb_group=exp002 task.env.n_controlled_robots=3;;
-6) python train.py task=VSSDecentralizedMA seed=10 experiment=ppo-dma wandb_group=exp002;;
-7) python train.py task=VSSDecentralizedMA seed=20 experiment=ppo-dma wandb_group=exp002;;
-8) python train.py task=VSSDecentralizedMA seed=30 experiment=ppo-dma wandb_group=exp002;;
+0) python train.py task=VSS seed=10 experiment=ppo wandb_group=exp004;;
+3) python train.py task=VSS seed=10 experiment=ppo-cma wandb_group=exp004 task.env.n_controlled_robots=3;;
+6) python train.py task=VSSDecentralizedMA seed=10 experiment=ppo-dma wandb_group=exp004;;
+1) python train.py task=VSS seed=20 experiment=ppo wandb_group=exp004;;
+4) python train.py task=VSS seed=20 experiment=ppo-cma wandb_group=exp004 task.env.n_controlled_robots=3;;
+7) python train.py task=VSSDecentralizedMA seed=20 experiment=ppo-dma wandb_group=exp004;;
+2) python train.py task=VSS seed=30 experiment=ppo wandb_group=exp004;;
+5) python train.py task=VSS seed=30 experiment=ppo-cma wandb_group=exp004 task.env.n_controlled_robots=3;;
+8) python train.py task=VSSDecentralizedMA seed=30 experiment=ppo-dma wandb_group=exp004;;
 *) echo "Opcao Invalida!" ;;
 esac
