26d6d43480bcccf64b3cc32a36b584e280715cb1
Felipe Martins <felipemartins96@gmail.com>
2023-02-17 16:42:00 -0300

    revert to exp001 setup

--- isaacgymenvs/tasks/rsoccer/vss_dma.py
+++ isaacgymenvs/tasks/rsoccer/vss_dma.py
@@ -61,7 +61,7 @@ class VSSDMA(VecTask):
         self.ou_theta = 0.1
         self.ou_sigma = 0.2
         self.step_count = 0
-        self.speed_factor = 0.0
+        self.speed_factor = 1.0
 
         self.n_team_actions = self.n_robots_per_team * self.n_robot_dofs
         self.cfg['env']['numActions'] = self.n_robot_dofs
@@ -157,7 +157,7 @@ class VSSDMA(VecTask):
         self.step_count += 1
         self.extras["step_count"] = self.step_count
         self.extras['speed_factor'] = self.speed_factor
-        self.speed_factor = min(1.0, self.step_count / self.cfg['env']['rand_speed_scheduler_max_step'])
+        # self.speed_factor = min(1.0, self.step_count / self.cfg['env']['rand_speed_scheduler_max_step'])
 
         self.compute_rewards_and_dones()
 
@@ -329,7 +329,7 @@ class VSSDMA(VecTask):
                         requires_grad=False,
                     )
                     - 0.5
-                ) * 1
+                ) * 2
                 self.ball_vel[field_ids] = rand_ball_vel * self.speed_factor
 
             self.gym.set_actor_root_state_tensor(
