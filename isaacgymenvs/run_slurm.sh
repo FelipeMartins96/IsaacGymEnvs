21fdc50b3f32ecf33878da2532793a212bceabaa
Felipe Martins <fbm2@cin.ufpe.br>
2023-02-21 10:58:43 -0300

    experiment 7: removing speed change ablation

--- isaacgymenvs/run_slurm.sh
+++ isaacgymenvs/run_slurm.sh
@@ -1,12 +1,12 @@
 case $1 in
-0) python train.py task=VSS seed=10 experiment=ppo wandb_group=exp006;;
-1) python train.py task=VSS seed=10 experiment=ppo-cma wandb_group=exp006 task.env.n_controlled_robots=3;;
-2) python train.py task=VSSDecentralizedMA seed=10 experiment=ppo-dma wandb_group=exp006;;
-3) python train.py task=VSS seed=20 experiment=ppo wandb_group=exp006;;
-4) python train.py task=VSS seed=20 experiment=ppo-cma wandb_group=exp006 task.env.n_controlled_robots=3;;
-5) python train.py task=VSSDecentralizedMA seed=20 experiment=ppo-dma wandb_group=exp006;;
-6) python train.py task=VSS seed=30 experiment=ppo wandb_group=exp006;;
-7) python train.py task=VSS seed=30 experiment=ppo-cma wandb_group=exp006 task.env.n_controlled_robots=3;;
-8) python train.py task=VSSDecentralizedMA seed=30 experiment=ppo-dma wandb_group=exp006;;
+0) python train.py task=VSS seed=10 experiment=ppo wandb_group=exp007;;
+1) python train.py task=VSS seed=10 experiment=ppo-cma wandb_group=exp007 task.env.n_controlled_robots=3;;
+2) python train.py task=VSSDecentralizedMA seed=10 experiment=ppo-dma wandb_group=exp007;;
+3) python train.py task=VSS seed=20 experiment=ppo wandb_group=exp007;;
+4) python train.py task=VSS seed=20 experiment=ppo-cma wandb_group=exp007 task.env.n_controlled_robots=3;;
+5) python train.py task=VSSDecentralizedMA seed=20 experiment=ppo-dma wandb_group=exp007;;
+6) python train.py task=VSS seed=30 experiment=ppo wandb_group=exp007;;
+7) python train.py task=VSS seed=30 experiment=ppo-cma wandb_group=exp007 task.env.n_controlled_robots=3;;
+8) python train.py task=VSSDecentralizedMA seed=30 experiment=ppo-dma wandb_group=exp007;;
 *) echo "Opcao Invalida!" ;;
 esac
--- isaacgymenvs/tasks/rsoccer/vss.py
+++ isaacgymenvs/tasks/rsoccer/vss.py
@@ -52,7 +52,7 @@ class VSS(VecTask):
         self.ou_theta = 0.1
         self.ou_sigma = 0.2
         self.step_count = 0
-        self.speed_factor = 0.0
+        self.speed_factor = 1.0
 
         self.n_team_actions = self.n_robots_per_team * self.n_robot_dofs
         self.cfg['env']['numActions'] = self.n_controlled_robots * self.n_robot_dofs
@@ -138,7 +138,7 @@ class VSS(VecTask):
         self.step_count += 1
         self.extras["step_count"] = self.step_count
         self.extras['speed_factor'] = self.speed_factor
-        self.speed_factor = min(1.0, self.step_count / self.cfg['env']['rand_speed_scheduler_max_step'])
+        # self.speed_factor = min(1.0, self.step_count / self.cfg['env']['rand_speed_scheduler_max_step'])
 
         self.compute_rewards_and_dones()
 
