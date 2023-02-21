156bf23957a5dc715e9f02b294cf5a8460414265
Felipe Martins <fbm2@cin.ufpe.br>
2023-02-21 11:01:28 -0300

    Revert "experiment 8: removing robot physics change ablation"
    This reverts commit 4d2db0cb3a7cac2c87edff52a6e28adc4e3d70c9.

--- isaacgymenvs/run_slurm.sh
+++ isaacgymenvs/run_slurm.sh
@@ -1,12 +1,12 @@
 case $1 in
-0) python train.py task=VSS seed=10 experiment=ppo wandb_group=exp008;;
-1) python train.py task=VSS seed=10 experiment=ppo-cma wandb_group=exp008 task.env.n_controlled_robots=3;;
-2) python train.py task=VSSDecentralizedMA seed=10 experiment=ppo-dma wandb_group=exp008;;
-3) python train.py task=VSS seed=20 experiment=ppo wandb_group=exp008;;
-4) python train.py task=VSS seed=20 experiment=ppo-cma wandb_group=exp008 task.env.n_controlled_robots=3;;
-5) python train.py task=VSSDecentralizedMA seed=20 experiment=ppo-dma wandb_group=exp008;;
-6) python train.py task=VSS seed=30 experiment=ppo wandb_group=exp008;;
-7) python train.py task=VSS seed=30 experiment=ppo-cma wandb_group=exp008 task.env.n_controlled_robots=3;;
-8) python train.py task=VSSDecentralizedMA seed=30 experiment=ppo-dma wandb_group=exp008;;
+0) python train.py task=VSS seed=10 experiment=ppo wandb_group=exp006;;
+1) python train.py task=VSS seed=10 experiment=ppo-cma wandb_group=exp006 task.env.n_controlled_robots=3;;
+2) python train.py task=VSSDecentralizedMA seed=10 experiment=ppo-dma wandb_group=exp006;;
+3) python train.py task=VSS seed=20 experiment=ppo wandb_group=exp006;;
+4) python train.py task=VSS seed=20 experiment=ppo-cma wandb_group=exp006 task.env.n_controlled_robots=3;;
+5) python train.py task=VSSDecentralizedMA seed=20 experiment=ppo-dma wandb_group=exp006;;
+6) python train.py task=VSS seed=30 experiment=ppo wandb_group=exp006;;
+7) python train.py task=VSS seed=30 experiment=ppo-cma wandb_group=exp006 task.env.n_controlled_robots=3;;
+8) python train.py task=VSSDecentralizedMA seed=30 experiment=ppo-dma wandb_group=exp006;;
 *) echo "Opcao Invalida!" ;;
 esac
