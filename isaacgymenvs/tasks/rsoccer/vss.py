411fbe10f52f57386cbcf9797a5e4f1d0a1ded5c
Felipe Martins <felipemartins96@gmail.com>
2023-02-27 14:32:01 -0300

    New Baseline, exp002 with speed factor removed

--- isaacgymenvs/tasks/rsoccer/vss.py
+++ isaacgymenvs/tasks/rsoccer/vss.py
@@ -52,7 +52,6 @@ class VSS(VecTask):
         self.ou_theta = 0.1
         self.ou_sigma = 0.2
         self.step_count = 0
-        self.speed_factor = 0.0
 
         self.n_team_actions = self.n_robots_per_team * self.n_robot_dofs
         self.cfg['env']['numActions'] = self.n_controlled_robots * self.n_robot_dofs
@@ -126,7 +125,7 @@ class VSS(VecTask):
                     requires_grad=False,
                 )
             )
-            self.ou_buffer = torch.clamp(self.ou_buffer, -1.0, 1.0) * self.speed_factor
+            self.ou_buffer = torch.clamp(self.ou_buffer, -1.0, 1.0)
             self.dof_velocity_buf[..., self.num_actions:] = self.ou_buffer
 
 
@@ -137,8 +136,6 @@ class VSS(VecTask):
         self.progress_buf += 1
         self.step_count += 1
         self.extras["step_count"] = self.step_count
-        self.extras['speed_factor'] = self.speed_factor
-        self.speed_factor = min(1.0, self.step_count / self.cfg['env']['rand_speed_scheduler_max_step'])
 
         self.compute_rewards_and_dones()
 
@@ -282,7 +279,7 @@ class VSS(VecTask):
                     )
                     - 0.5
                 ) * 1
-                self.ball_vel[env_ids] = rand_ball_vel * self.speed_factor
+                self.ball_vel[env_ids] = rand_ball_vel
 
             self.gym.set_actor_root_state_tensor(
                 self.sim, gymtorch.unwrap_tensor(self.root_state)
