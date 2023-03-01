7bde4bce9f0ff65d74fe6833c1c479126183a02e
Felipe Martins <felipemartins96@gmail.com>
2023-03-01 10:26:17 -0300

    fix exp015 scripts

--- isaacgymenvs/run_slurm.sh
+++ isaacgymenvs/run_slurm.sh
@@ -1,12 +1,12 @@
 case $1 in
-0) python train.py seed=10 wandb_group=exp014 experiment=ppo        task=VSS                task.env.has_energy=False task.env.has_move=False;;
-1) python train.py seed=10 wandb_group=exp014 experiment=ppo-cma    task=VSS                task.env.has_energy=False task.env.has_move=False  task.env.n_controlled_robots=3;;
-2) python train.py seed=10 wandb_group=exp014 experiment=ppo-dma    task=VSSDecentralizedMA task.env.has_energy=False task.env.has_move=False;;
-3) python train.py seed=20 wandb_group=exp014 experiment=ppo        task=VSS                task.env.has_energy=False task.env.has_move=False;;
-4) python train.py seed=20 wandb_group=exp014 experiment=ppo-cma    task=VSS                task.env.has_energy=False task.env.has_move=False  task.env.n_controlled_robots=3;;
-5) python train.py seed=20 wandb_group=exp014 experiment=ppo-dma    task=VSSDecentralizedMA task.env.has_energy=False task.env.has_move=False;;
-6) python train.py seed=30 wandb_group=exp014 experiment=ppo        task=VSS                task.env.has_energy=False task.env.has_move=False;;
-7) python train.py seed=30 wandb_group=exp014 experiment=ppo-cma    task=VSS                task.env.has_energy=False task.env.has_move=False  task.env.n_controlled_robots=3;;
-8) python train.py seed=30 wandb_group=exp014 experiment=ppo-dma    task=VSSDecentralizedMA task.env.has_energy=False task.env.has_move=False;;
+0) python train.py seed=10 wandb_group=exp015 experiment=ppo        task=VSS                task.env.has_energy=False task.env.has_move=False;;
+1) python train.py seed=10 wandb_group=exp015 experiment=ppo-cma    task=VSS                task.env.has_energy=False task.env.has_move=False  task.env.n_controlled_robots=3;;
+2) python train.py seed=10 wandb_group=exp015 experiment=ppo-dma    task=VSSDecentralizedMA task.env.has_energy=False task.env.has_move=False;;
+3) python train.py seed=20 wandb_group=exp015 experiment=ppo        task=VSS                task.env.has_energy=False task.env.has_move=False;;
+4) python train.py seed=20 wandb_group=exp015 experiment=ppo-cma    task=VSS                task.env.has_energy=False task.env.has_move=False  task.env.n_controlled_robots=3;;
+5) python train.py seed=20 wandb_group=exp015 experiment=ppo-dma    task=VSSDecentralizedMA task.env.has_energy=False task.env.has_move=False;;
+6) python train.py seed=30 wandb_group=exp015 experiment=ppo        task=VSS                task.env.has_energy=False task.env.has_move=False;;
+7) python train.py seed=30 wandb_group=exp015 experiment=ppo-cma    task=VSS                task.env.has_energy=False task.env.has_move=False  task.env.n_controlled_robots=3;;
+8) python train.py seed=30 wandb_group=exp015 experiment=ppo-dma    task=VSSDecentralizedMA task.env.has_energy=False task.env.has_move=False;;
 *) echo "Opcao Invalida!" ;;
 esac
