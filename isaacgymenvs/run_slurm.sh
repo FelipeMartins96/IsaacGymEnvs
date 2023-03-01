f4ef67cad38e88ded00032a4f64b01b3fd695842
Felipe Martins <felipemartins96@gmail.com>
2023-02-28 23:52:50 -0300

    exp014: same as exp011, with entropy coef
    formatting

--- isaacgymenvs/run_slurm.sh
+++ isaacgymenvs/run_slurm.sh
@@ -1,12 +1,12 @@
 case $1 in
-0) python train.py task=VSS seed=10 experiment=ppo wandb_group=exp013 train.params.config.kl_threshold=0.016 task.env.has_move=False;;
-1) python train.py task=VSS seed=10 experiment=ppo-cma wandb_group=exp013 train.params.config.kl_threshold=0.016 task.env.n_controlled_robots=3 task.env.has_move=False;;
-2) python train.py task=VSSDecentralizedMA seed=10 experiment=ppo-dma wandb_group=exp013 train.params.config.kl_threshold=0.016 task.env.has_move=False;;
-3) python train.py task=VSS seed=20 experiment=ppo wandb_group=exp013 train.params.config.kl_threshold=0.016 task.env.has_move=False;;
-4) python train.py task=VSS seed=20 experiment=ppo-cma wandb_group=exp013 train.params.config.kl_threshold=0.016 task.env.n_controlled_robots=3 task.env.has_move=False;;
-5) python train.py task=VSSDecentralizedMA seed=20 experiment=ppo-dma wandb_group=exp013 train.params.config.kl_threshold=0.016 task.env.has_move=False;;
-6) python train.py task=VSS seed=30 experiment=ppo wandb_group=exp013 train.params.config.kl_threshold=0.016 task.env.has_move=False;;
-7) python train.py task=VSS seed=30 experiment=ppo-cma wandb_group=exp013 train.params.config.kl_threshold=0.016 task.env.n_controlled_robots=3 task.env.has_move=False;;
-8) python train.py task=VSSDecentralizedMA seed=30 experiment=ppo-dma wandb_group=exp013 train.params.config.kl_threshold=0.016 task.env.has_move=False;;
+0) python train.py seed=10 wandb_group=exp014 experiment=ppo        task=VSS                train.params.config.entropy_coef=0.001 task.env.has_move=False;;
+1) python train.py seed=10 wandb_group=exp014 experiment=ppo-cma    task=VSS                train.params.config.entropy_coef=0.001 task.env.has_move=False  task.env.n_controlled_robots=3;;
+2) python train.py seed=10 wandb_group=exp014 experiment=ppo-dma    task=VSSDecentralizedMA train.params.config.entropy_coef=0.001 task.env.has_move=False;;
+3) python train.py seed=20 wandb_group=exp014 experiment=ppo        task=VSS                train.params.config.entropy_coef=0.001 task.env.has_move=False;;
+4) python train.py seed=20 wandb_group=exp014 experiment=ppo-cma    task=VSS                train.params.config.entropy_coef=0.001 task.env.has_move=False  task.env.n_controlled_robots=3;;
+5) python train.py seed=20 wandb_group=exp014 experiment=ppo-dma    task=VSSDecentralizedMA train.params.config.entropy_coef=0.001 task.env.has_move=False;;
+6) python train.py seed=30 wandb_group=exp014 experiment=ppo        task=VSS                train.params.config.entropy_coef=0.001 task.env.has_move=False;;
+7) python train.py seed=30 wandb_group=exp014 experiment=ppo-cma    task=VSS                train.params.config.entropy_coef=0.001 task.env.has_move=False  task.env.n_controlled_robots=3;;
+8) python train.py seed=30 wandb_group=exp014 experiment=ppo-dma    task=VSSDecentralizedMA train.params.config.entropy_coef=0.001 task.env.has_move=False;;
 *) echo "Opcao Invalida!" ;;
 esac
